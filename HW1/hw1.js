63383def18ab91ed319a5d873f017c1cb2e383c0
katerina-serhieieva <kate.sergeeva2302@gmail.com>
2025-02-04 23:22:15 +0200

    Initial commit

---
+++ HW1/hw1.js
@@ -0,0 +1,43 @@
+let a = 'hello'
+console.log(a)
+let b = 'owu'
+console.log(b)
+let c = 'com'
+console.log(c)
+let d = 'ua'
+console.log(d)
+
+let e = 1
+console.log(e)
+let f = 10
+console.log(f)
+let g = -999
+console.log(g)
+let h = 123
+console.log(h)
+let i = 3.14
+console.log(i)
+let k = 2.7
+console.log(k)
+let l = 16
+console.log(l)
+
+let m = true
+console.log(m)
+let n = false
+console.log(n)
+
+let firstName = `Katerina`
+let middleName = `Olehivna`
+let lastName = `Serhieieva`
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+let o = 100; let p = '100'; let q = true;
+console.log(typeof o);
+console.log(typeof p);
+console.log(typeof q);
+
+
+
+
